074885cb76df29600676dd54271c0a120b71cf19
gustavohnrq <91096651+gustavohnrq@users.noreply.github.com>
2026-03-04 11:39:47 -0300

    Restore functional Form_Menu dashboard version provided by user

--- Triggers.js
+++ Triggers.js
@@ -16,3 +16,23 @@ function dailyMetricsJob_(){
   rebuildControleSemanal();
   rebuildFunilMensal();
 }
+
+function installDailyFollowUpTrigger(){
+  if (typeof FU_installDailyTrigger_6am !== 'function') {
+    throw new Error('FollowUpService não carregado.');
+  }
+  return FU_installDailyTrigger_6am();
+}
+
+function runDailyFollowUpNow(){
+  if (typeof FU_dailyFollowUpJob_ !== 'function') {
+    throw new Error('FollowUpService não carregado.');
+  }
+  return FU_dailyFollowUpJob_();
+}
+
+function installAllAutomationTriggers(){
+  const metrics = installDailyMetricsTrigger();
+  const follow = installDailyFollowUpTrigger();
+  return { ok: true, metrics, follow };
+}
